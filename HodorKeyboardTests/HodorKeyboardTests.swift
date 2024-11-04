b722d98fc104887f5191c9b2b864c165d70adec9
habildem <habildmrc@gmail.com>
2024-11-05 01:18:01 +0300

    HODOR!

---
+++ HodorKeyboardTests/HodorKeyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  HodorKeyboardTests.swift
+//  HodorKeyboardTests
+//
+//  Created by Habil Demirci on 5.11.2024.
+//
+
+import Testing
+@testable import HodorKeyboard
+
+struct HodorKeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
